9f3d5be6459d004fdca42314b0a6c08af3ba4799
Asdfif <sasha-burov@mail.ru>
2021-03-16 22:36:24 +0300

    Реализовано хранение файлов через Amazon S3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::Base   
   def delete_file_from_resource(resource, file_id)
     resource.files.find(file_id).purge
   end
