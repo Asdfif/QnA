45c45c9210712eff16c28c9b0395efa3df27ee67
Asdfif <sasha-burov@mail.ru>
2021-04-06 18:07:31 +0300

    Добавлен гем active_model_serializer, добавлен question_seriazlizer

--- config/routes.rb
+++ config/routes.rb
@@ -34,9 +34,10 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :profiles, only:[] do
+      resources :profiles, only: [] do
         get :me, on: :collection
       end
+      resources :questions, only: %i[index]
     end
   end
 
